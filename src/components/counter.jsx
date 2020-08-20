a19efd2fd00ef8f8eeb10134b9b85ffacf92228d
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2020-08-20 15:54:55 +0530

    component embedded

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
     return (
       <React.Fragment>
-        <h1> Hello There </h1>
+        <span>{this.formatCount()}</span>
+        <br />
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    // You can return jsx expresssion like <h1>Zero</h1>
+    return count === 0 ? "Zero" : count;
   }
 }
 
