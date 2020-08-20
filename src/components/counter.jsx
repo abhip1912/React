3b52be0d1d88339a27210f601d93db1989f817f7
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2020-08-20 17:26:37 +0530

    rendering condition

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,42 +3,20 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: ["tag1", 23],
   };
-  // styles = {
-  //   fontSize: 20,
-  //   fontWeight: "bold",
-  // };
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is no tags !</p>;
     return (
-      // <React.Fragment>
-      <div>
-        {/* <img src={this.state.imageUrl} alt="RandomPic" /> */}
-        {/* <span style={this.styles} className="badge badge-primary m-2"> */}
-        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    // You can return jsx expresssion like <h1>Zero</h1>
-    return count === 0 ? "Zero" : count;
+  render() {
+    return <div>{this.renderTags()}</div>;
   }
 }
 
